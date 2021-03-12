735bf802db3375743fe05d8cf20a6bf72790ddef
vladimiremi <vladimiremi2014@gmail.com>
2021-03-11 22:17:04 -0300

    chore: add login page

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="login">
+      <div className="login-container">
+        <div>
+          <h1>Seja bem-vindo</h1>
+          <p>Para continuar</p>
+          <p>por favor faça login</p>
+
+          <h5>Logotipo</h5>
+        </div>
+        <div>
+          <section>
+            <p>Utilize o painel agora</p>
+            <p>mesmo</p>
+            <form action="">
+              <input type="e-mail" placeholder="Digite seu e-mail" />
+              <input type="e-mail" placeholder="Digite sua senha" />
+              <button>Entrar</button>
+            </form>
+          </section>
+
+        </div>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
