d887252d9c18d8de046b9589f136292c94613d5f
vladimiremi <vladimiremi2014@gmail.com>
2021-03-15 16:17:20 -0300

    feat: add drawer

---
+++ src/components/Drawer/Drawer.js
@@ -0,0 +1,21 @@
+import { IoMdMenu } from 'react-icons/io';
+import './styles.css';
+
+export default function Drawer() {
+    return (
+        <div className="drawer">
+            <div className="icon-drawer">
+
+                <IoMdMenu fontSize="6vh" color="rgb(0, 102, 240)"  />
+
+                <p >Listagem de colaboradores</p>
+            </div>
+            <div className="icon-drawer">
+                <IoMdMenu fontSize="6vh" color="rgb(0, 102, 240)" />
+                
+                <p>Adicionar um colaborador</p>
+            </div>
+            
+        </div>
+    )
+}
--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,9 +1,42 @@
 import Bar from "../../components/Bar/Bar";
-
+import Drawer from "../../components/Drawer/Drawer";
+import './styles.css';
 export default function List() {
     return(
-        <>
+        <div className='list-container'>
             <Bar />
-        </>
+            <Drawer />
+            <body className="body">
+                <section>
+                    <p>lista</p>
+                    <ul>
+                        <li>
+                            Vladimir
+                        </li>
+                        <li>
+                            Vladimir
+                        </li>
+                        <li>
+                            Vladimir
+                        </li>
+                        <li>
+                            Vladimir
+                        </li>
+                        <li>
+                            Vladimir
+                        </li>
+                        <li>
+                            Vladimir
+                        </li>
+                        <li>
+                            Vladimir
+                        </li>
+                    </ul>
+
+                </section>
+                
+
+            </body>
+        </div>
     )
 }
